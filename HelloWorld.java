c33503a8e6d255c564d496e35f404dd4d382dc65
amol kalugade <amol.kalugade@gmail.com>
2022-01-24 15:04:39 +0530

    checking SCM

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,8 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
          System.out.println("new commit");
+	  System.out.println("trying POLL SCM");
+
 	
     }
 }
